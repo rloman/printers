28760889e274cb6c8290e6a160cca9be159b0c30
Raymond Loman <raymond@carpago.nl>
2020-01-27 09:57:21 +0100

    Init

---
+++ src/test/java/nl/capgemini/printers/PrintersApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.capgemini.printers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
