d0a4eaf75b418ba62fc2d782d507265b0e98a2b1
Raymond Loman <raymond@carpago.nl>
2020-01-28 23:21:57 +0100

    Set some var to let

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,10 @@
-function print() {
-}
 const api = "http://localhost:8080/api/printers";
 
 function getData(){
-          var xhttp = new XMLHttpRequest();
+          let xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function() {
                   if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("demo").innerHTML = this.responseText;
+                   document.getElementById("demo").innerHTML = this.responseText;
                    parseJson(this.responseText);
                   }
                 };
@@ -16,28 +14,30 @@ function getData(){
 }
 
  function sendPrinter(){
-                var type = document.getElementById("type").value;
-                var price = document.getElementById("price").value;
+                let type = document.getElementById("type").value;
+                let price = document.getElementById("price").value;
 
-                var printer = {type:type, price:price};
+                let printer = {
+                    type:type,
+                    price:price};
                 postData(JSON.stringify(printer));
  }
 
  function postData(data){
      var xhttp = new XMLHttpRequest();
                  xhttp.onreadystatechange = function() {
-                         if (this.readyState == 4 && this.status == 201) {
+                         if (this.readyState == 4 && this.status == 200) {
                            document.getElementById("demo").innerHTML = this.responseText;
                          }
                  };
                  xhttp.open("POST", api, true);
                  xhttp.setRequestHeader("Content-type", "application/json");
-                 xhttp.send(data);
+                 xhttp.send(data);// data is STring hiere!
          }
 
-function parseJson(data) {
+function parseJson(dataString) {
     // hopefully data contains a String containing a list of printers
-    let printers = JSON.parse(data);
+    let printers = JSON.parse(dataString);
 
     for(let printer of printers) {
         console.log(`found a printer with id: ${printer.id} and price:${printer.price} and type:${printer.type}`);
